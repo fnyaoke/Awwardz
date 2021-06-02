02553934d10fc242d2396ca49246bfa810fb9a56
fnyaoke <fnyaoke@gmail.com>
2021-06-02 15:52:45 +0300

    created serializers

---
+++ award/serializers.py
@@ -0,0 +1,14 @@
+from .models import Post, Profile
+from rest_framework import serializers
+
+
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('title', 'image', 'description', 'link')
+
+
+class ProfileSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Profile
+        fields = (' First_Name', 'Last_Name', ' Email', ' bio', ' profile_pic')
